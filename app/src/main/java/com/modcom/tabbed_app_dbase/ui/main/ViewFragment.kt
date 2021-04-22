187934b988b2159aff6b1816aa5d7677bbe28d01
modcomlearning <modcomlearning@gmail.com>
2021-04-22 10:53:02 +0300

    First Commit

--- app/src/main/java/com/modcom/tabbed_app_dbase/ui/main/ViewFragment.kt
+++ app/src/main/java/com/modcom/tabbed_app_dbase/ui/main/ViewFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
 import com.modcom.tabbed_app_dbase.R
+import cz.msebera.android.httpclient.Header
 
 
 class ViewFragment : Fragment() {
@@ -16,11 +21,42 @@ class ViewFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         //front end  - what the users see
-
+        //specify the url with the data
         val url = "http://localhost/api/view.php";
 
+        //Front End side
+        val root = inflater.inflate(R.layout.fragment_view, container, false)
 
-        return inflater.inflate(R.layout.fragment_view, container, false)
+        //client a client object
+        val client = AsyncHttpClient(true,80,443)
+
+        //below object to be used in case we want to submit something to the server
+        val parameters =  RequestParams()
+
+        //Now use AsyncHttpClient to connect to your APi - url
+        client.post(url, parameters, object: AsyncHttpResponseHandler()
+        {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray) {
+                //convert Byte array to string
+                val response = String(responseBody)
+                //find your textview
+                val results = root.findViewById<TextView>(R.id.results)
+                //Put the response to the textview
+                results.text = response
+
+
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                val results = root.findViewById<TextView>(R.id.results)
+                results.text = "Server Error Encountered"
+            }
+            
+        }
+        )//post
+
+
+        return root
     }
 
 }//end fragment
