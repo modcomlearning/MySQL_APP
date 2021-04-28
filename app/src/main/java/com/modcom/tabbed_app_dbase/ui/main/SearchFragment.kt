684edb61f70d670390f17809496bd19b3a710bf2
modcomlearning <modcomlearning@gmail.com>
2021-04-28 10:53:18 +0300

    Fourth Commit

--- app/src/main/java/com/modcom/tabbed_app_dbase/ui/main/SearchFragment.kt
+++ app/src/main/java/com/modcom/tabbed_app_dbase/ui/main/SearchFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.modcom.tabbed_app_dbase.R
 
 
@@ -14,8 +16,10 @@ class SearchFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_search, container, false)
-        
+        val make = root.findViewById<EditText>(R.id.edit_search_make)
+        val search = root.findViewById<Button>(R.id.btn_search)
 
+        
 
         return root
     }
