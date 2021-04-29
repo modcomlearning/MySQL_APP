9b36b252569134d1701670e142c695b56387a7f1
modcomlearning <modcomlearning@gmail.com>
2021-04-29 11:00:37 +0300

    Fifth Commit

--- app/src/main/java/com/modcom/tabbed_app_dbase/Login.kt
+++ app/src/main/java/com/modcom/tabbed_app_dbase/Login.kt
@@ -2,10 +2,38 @@ package com.modcom.tabbed_app_dbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.RequestParams
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //this an activity
+
+        btn_login.setOnClickListener{
+
+            val client = AsyncHttpClient(true, 80,443)
+            val parameter = RequestParams()
+            parameter.add("username", edit_username.text.toString())
+            parameter.add("password", edit_password.text.toString())
+
+            val url = "http://10.0.2.2/api/login.php";
+
+
+            
+
+
+
+
+
+
+
+
+
+
+        }//end listner
     }
 }
