5b2e4610ba1575620faba3926f9c307f9eed4b1b
modcomlearning <modcomlearning@gmail.com>
2021-04-29 11:19:59 +0300

    Fifth Commit

--- app/src/main/java/com/modcom/tabbed_app_dbase/Login.kt
+++ app/src/main/java/com/modcom/tabbed_app_dbase/Login.kt
@@ -1,5 +1,6 @@
 package com.modcom.tabbed_app_dbase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -26,14 +27,24 @@ class Login : AppCompatActivity() {
 
             client.post(url, parameter, object: AsyncHttpResponseHandler()
             {
-                override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
-                    TODO("Not yet implemented")
-                }
+                override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray) {
+                    val response = String(responseBody)
+                    if (response.startsWith("1")){
+                        //successful
+                        val x = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(x)
+                        finish() //kill login as you move to main
+                    }
+                    
+                    else{
+                        Toast.makeText(applicationContext, "Login Failed", Toast.LENGTH_LONG).show()
+                    }
+
+                }//end on success
 
                 override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
                     Toast.makeText(applicationContext, "Failed. Network Error", Toast.LENGTH_LONG).show()
-                }
-
+                }//end on failure
             }
             )//end post
 
